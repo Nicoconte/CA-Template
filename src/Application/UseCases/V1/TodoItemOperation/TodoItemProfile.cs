0e636911d2783a099e750bc996e84cfa5b901c98
NicoConte <nicoconte1999@gmail.com>
2024-03-23 17:22:17 -0300

    refactor: Move Profile and dto into todoItemOperation

---
+++ src/Application/UseCases/V1/TodoItemOperation/TodoItemProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Domain.Entities;
+
+namespace Application.UseCases.V1.TodoItemOperation
+{
+    public class TodoItemProfile : Profile
+    {
+        public TodoItemProfile()
+        {
+            CreateMap<TodoItem, TodoItemDto>();
+        }
+    }
+}
